7ada2245876793e8c42b58663a418e76719217ba
Varis Darasirikul <varis.dara@gmail.com>
2020-01-22 23:41:43 +0800

    Add pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -38,8 +38,41 @@ export const getBootcamps = asyncHandler(async function(req, res, next) {
     query = query.sort("-createdAt");
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await BootcampModel.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  // Executing query
   const bootcamps = await query;
-  res.status(200).json({ success: true, data: bootcamps });
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps
+  });
 });
 
 // @access Public
